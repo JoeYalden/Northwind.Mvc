26207117cf6e95e05e7a261521b6aee2ba382ef8
JoeYalden <113351008+JoeYalden@users.noreply.github.com>
2023-11-06 11:35:51 +0000

    Updated some shtuff

---
+++ Northwind.Common.DataContext.SqlServer/NorthwindContext.cs
@@ -0,0 +1,23 @@
+﻿namespace Packt.Shared
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Data.SqlClient;
+    public class NorthwindContext: DbContext
+    {
+        public NorthwindContext() { }
+            public NorthwindContext(DbContextOptions<NorthwindContext> options) : base(options) { }
+        
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            SqlConnectionStringBuilder builder = new();
+            builder.DataSource = ".";
+            builder.InitialCatalog = "Northwind";
+            builder.IntegratedSecurity = false;
+            builder.UserID = "jkoe";
+            builder.Password = "password";
+            builder.TrustServerCertificate = true;
+            builder.MultipleActiveResultSets = true;
+            builder.CommandTimeout = 3;
+            string? connection = builder.ConnectionString;
+        }
+    }
+}
